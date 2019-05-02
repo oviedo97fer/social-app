60c1e383acddcd24c26d3495f12e13a5c5796ad7
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-05-01 23:48:50 -0300

    conectando DB de firebase para subir imagenes

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -8,15 +8,22 @@ class App extends React.Component{
     super();
     this.state = {
       user: null,
+      pictures: []
     };
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleUpload = this.handleUpload.bind(this)
   }
 
   componentWillMount(){
     firebase.auth().onAuthStateChanged(user=>{
       this.setState({
         user
+      })
+    });
+    firebase.database().ref('pictures').on('child_added', snapshot=>{
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
       })
     })
   }
@@ -33,6 +40,29 @@ class App extends React.Component{
       .then(result=>console.log(`${result.user.email} ha cerrado sesión`))
       .catch(error=>console.log(`Error ${error.code}: ${error.message}`))
   }
+  handleUpload(event){
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/Photos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot =>{
+        let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({
+            uploadValue: percentage
+        })
+    }, error=>{ 
+        console.log(error.message,'hola');
+    }, ()=>{
+      const record = {
+                      photoURL: this.state.user.photoURL,
+                      displayName: this.state.user.displayName,
+                      image: task.snapshot.downloadURL
+                    };
+      const dbRef = firebase.database().ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+    });
+}
 
   renderLoginButton(){
     //if user's logg
@@ -42,7 +72,18 @@ class App extends React.Component{
           <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
           <p>Hola {this.state.user.displayName}!</p>
           <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload/>
+          <FileUpload onUpload={this.handleUpload}/>
+          {
+            this.state.pictures.map(picture=>(
+              <div>
+                <img src={picture.image}/>
+                <br/>
+                <img src={picture.photoURL} alt={picture.displayName}/>
+                <br/>
+                <span>{picture.displayName}</span>
+              </div>
+            ))
+          }
         </div>
       );
     }else{
